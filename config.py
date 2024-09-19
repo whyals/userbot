d4def2eb54a67649cad7970b26ddd3fefe5dbb21
why_als <123080909+whyals@users.noreply.github.com>
2024-09-19 21:34:45 +0300

    Create config.py
    добавил конфиг файл со всеми необходимыми ключами и параметрами

---
+++ config.py
@@ -0,0 +1,18 @@
+from telethon import TelegramClient
+
+API_ID = '...'
+API_HASH = '...'
+SESSION_NAME = 'session'
+
+
+OPENAI_API_KEY = '...'
+GPT_MODEL = "..." #gpt-4o-mini-2024-07-18 cамая дешевая 
+
+
+SP_CLIENT_ID = '...'
+SP_CLIENT_SECRET = '...'
+
+YM_TOKEN = '...'
+
+
+client = TelegramClient(SESSION_NAME, API_ID, API_HASH)
