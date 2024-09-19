da7d5f21d35ab446b1f8633e2d3ce08f1751a3c0
why_als <123080909+whyals@users.noreply.github.com>
2024-09-19 21:21:20 +0300

    Create yandex_music_search_func.py
    добавил модуль поиска в яндекс музыке

---
+++ utils/yandex_music_search_func.py
@@ -0,0 +1,21 @@
+from config import YM_TOKEN
+from yandex_music import ClientAsync
+
+client = ClientAsync(YM_TOKEN)
+
+async def get_yandex_music_url(query):
+    await client.init()
+
+    search_result = await client.search(query, type_="track")
+
+    if not search_result.tracks:
+        print("Песни не найдены.")
+        return None
+
+    track = search_result.tracks.results[0]
+    # artists = ", ".join([artist.name for artist in track.artists])
+    link = f"https://music.yandex.ru/track/{track.id}"
+    # cover_url = ("https://" + track.cover_uri.replace("%%", "400x400") if track.cover_uri else None)
+
+    print(link)
+    return link
