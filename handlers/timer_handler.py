4a28f973c55a4410bfff72dc03a4d9c0198d49c0
why_als <123080909+whyals@users.noreply.github.com>
2024-10-13 05:44:38 +0300

    Update timer_handler.py
    доработал таймер и добавил разные форматы времени

--- handlers/timer_handler.py
+++ handlers/timer_handler.py
@@ -1,20 +1,63 @@
-from telethon.sync import events
 import asyncio
+import re
+import logging
 
-@events.register(events.NewMessage(pattern=r'\?timer (\d+)'))
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+timer_running = False
+
+
 async def timer_handler(event):
+    global timer_running
     try:
-        seconds = int(event.pattern_match.group(1))
+        message = event.pattern_match.group(1)
+        logger.info(f"Запрос таймера на {message}")
 
-        if seconds <= 0:
-            await event.reply("Укажите положительное количество секунд.")
+        if message.lower() == "stop":
+            await event.delete()
+            timer_running = False
+            logger.info("Таймер остановлен")
             return
 
-        for remaining in range(seconds, 0, -1):
-            await event.edit(f'{remaining} ')
+        hms_match = re.match(r'(\d+):(\d+):(\d+)', message)
+        hm_match = re.match(r'(\d+):(\d+)', message)
+        sec_match = re.match(r'(\d+)', message)
+        logger.info(f"Результат: {hms_match} {hm_match} {sec_match}")
+
+        if hms_match:
+            hours = int(hms_match.group(1))
+            minutes = int(hms_match.group(2))
+            seconds = int(hms_match.group(3))
+            total_seconds = hours * 3600 + minutes * 60 + seconds
+        elif hm_match:
+            hours = int(hm_match.group(1))
+            minutes = int(hm_match.group(2))
+            total_seconds = hours * 3600 + minutes * 60
+        elif sec_match:
+            total_seconds = int(sec_match.group(1))
+        else:
+            await event.edit("Пожалуйста, укажите время в формате hh:mm:ss, hh:mm или в секундах")
+            return
+
+        if total_seconds <= 0:
+            await event.edit("Укажите положительное количество времени")
+            return
+
+        timer_running = True
+        for remaining in range(total_seconds, 0, -1):
+            if not timer_running:
+                await event.reply("Таймер остановлен")
+                logger.info("Таймер остановлен")
+                return
+
+            hours, remainder = divmod(remaining, 3600)
+            minutes, seconds = divmod(remainder, 60)
+            await event.edit(f'{hours:02}:{minutes:02}:{seconds:02}')
             await asyncio.sleep(1)
 
-        await event.edit("Alert!")
+        await event.reply("АЛЕРТ")
+        logger.info("Таймер закончился")
 
     except ValueError:
-        await event.reply("Пожалуйста, укажите корректное количество секунд.")
+        await event.edit("Пожалуйста, укажите корректное количество времени")
